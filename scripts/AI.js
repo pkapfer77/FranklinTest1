0bad219e5113bc731fecf03e6806edfbb16224e0
Philippe Kapfer <pkapfer@adobe.com>
2023-03-08 15:00:16 +0100

    change scripts to add AI

--- scripts/AI.js
+++ scripts/AI.js
@@ -7,7 +7,7 @@
     // https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
 
     // the link to your model provided by Teachable Machine export panel
-    const URL = "./my_model/";
+    const URL = "https://teachablemachine.withgoogle.com/models/vsiWFiDT9/";
 
     let model, webcam, labelContainer, maxPredictions;
 
@@ -32,10 +32,11 @@
 
         // append elements to the DOM
         document.getElementById("webcam-container").appendChild(webcam.canvas);
+        /*
         labelContainer = document.getElementById("label-container");
         for (let i = 0; i < maxPredictions; i++) { // and class labels
             labelContainer.appendChild(document.createElement("div"));
-        }
+        }*/
     }
 
     async function loop() {
